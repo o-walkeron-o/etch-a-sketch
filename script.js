63e79912cde894560bbdd0fad7c74d7281ac41a8
walkeron <177946022+o-walkeron-o@users.noreply.github.com>
2025-03-29 10:01:07 +0000

    Add a button to set the size of the grid
    - Wrapped the loop that creates squares into the createGridOfSquares
      function.
    - Added an event listener to the button that prompts the user for
      the grid size. After deleting the existing grid, the function is
      called again with the new size parameter.
    - Add styles to the button and the container.

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.querySelector(".container");
+const btnSetUpSize = document.querySelector("#btnSetUpSize");
 
 let gridSize = 16;
 let squareSize = 960 / gridSize;
 let numberOfSquares = gridSize * gridSize;
 
-for (let i = 0; i < numberOfSquares; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px;`);
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "yellow";
-    })
-    container.appendChild(square);
+function createGridOfSquares(numberOfSquares, squareSize) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px;`);
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        });
+        container.appendChild(square);
+    }
 }
+
+createGridOfSquares(numberOfSquares, squareSize);
+
+btnSetUpSize.addEventListener("click", () => {
+    do {
+        gridSize = +prompt("Type a number between 1 and 100 for the grid size")
+    } while (gridSize < 1 || gridSize > 100 || isNaN(gridSize));
+    squareSize = 960 / gridSize;
+    numberOfSquares = gridSize * gridSize;
+    container.textContent = "";
+    createGridOfSquares(numberOfSquares, squareSize);
+});
